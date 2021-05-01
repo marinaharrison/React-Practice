5b03e560cc9b0f25e7344b32ec930595707ad440
Marina Harrison <marinaharrison1@gmail.com>
2021-05-01 14:36:56 -0700

    first attempt

--- scoreboard/app.js
+++ scoreboard/app.js
@@ -1,1 +1,24 @@
 //REACT TREEHOUSE PRACTICE
+const title = React.createElement(
+ 'h1',
+ { id: 'main-title', title: 'this is a title' },
+ 'my first react element'
+);
+
+const desc = React.createElement(
+  'p',
+  null,
+  'I just learned how to create a react node and render it into the DOM' 
+);
+
+const header = React.createElement(
+   'header',
+    null,
+    title,
+    desc
+);
+
+ReactDOM.render(
+    header,
+    document.getElementById('root')
+);
