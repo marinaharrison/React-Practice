5d5ffe490f650f042aee7fcddd7ab5fee78f4d24
Marina Harrison <marinaharrison1@gmail.com>
2021-05-02 19:07:54 -0700

    adding components and states

--- scoreboard/app.js
+++ scoreboard/app.js
@@ -1,24 +1,103 @@
-//REACT TREEHOUSE PRACTICE
-const title = React.createElement(
- 'h1',
- { id: 'main-title', title: 'this is a title' },
- 'my first react element'
-);
+function Header(props){
+    return (
+       <header>
+          <h1>{ props.title }</h1>
+          <span className="stats">Players: { props.totalPlayers }</span> 
+       </header> 
+    );    
+}
 
-const desc = React.createElement(
-  'p',
-  null,
-  'I just learned how to create a react node and render it into the DOM' 
-);
+function Player(props) {
+    return (
+     <div className="player">
+        <span className="player-name">
+         {props.name}
+        </span>
 
-const header = React.createElement(
-   'header',
-    null,
-    title,
-    desc
-);
+        <Counter score={props.score} />
+     </div>   
+    );
+} 
+
+class Counter extends React.Component {
+        state = {
+          score: 0  
+        };
+
+      incrementScore() {
+          this.setState( prevState => {
+              return {
+                  score: prevState.score + 1
+              };
+          });
+        }
+
+        decrementScore() {
+            this.setState( prevState => {
+                return {
+                score: prevState.score - 1
+                }
+            });
+        }
+
+  render() {
+    return (
+        <div className="counter">
+        <button className="counter-action decrement" onClick={this.decrementScore.bind(this)}> - </button>
+        <span className="counter-score">{ this.state.score}</span>
+        <button className="counter-action increment" onClick={this.incrementScore.bind(this)}> + </button>
+    </div>
+        );  
+    }
+}
+
+class App extends React.Component {
+
+    state = {
+       players: [
+        {
+            name: "Marina",
+            score: 50,
+            id: 1
+          },
+          {
+            name: "Pia",
+            score: 85,
+            id: 2
+          },
+          {
+            name: "Raz",
+            score: 95,
+            id: 3
+          },
+          {
+            name: "Maria",
+            score: 80,
+            id: 4
+          }
+       ] 
+    };
+
+    render() {
+        return(
+            <div className="scoreboard">
+                <Header title="Scoreboard" totalPlayers={this.state.players.length} />
+      
+                {/* Player List */}
+                {this.state.players.map( player =>
+                 <Player
+                       name={player.name}
+                       score={player.score}
+                       key={player.id.toString()}
+                   /> 
+                 )}
+            </div>  
+          );
+      }
+    }
+
 
 ReactDOM.render(
-    header,
+    <App />,
     document.getElementById('root')
-);
+); 
